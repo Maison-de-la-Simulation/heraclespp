2abf36e0ee4c4596a558655c3aae402da24053c5
lrousselhard <lou.roussel-hard@cea.fr>
2023-01-04 16:33:04 +0100

    Main

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,24 @@
 #include <iostream>
 #include <Kokkos_Core.hpp>
 
+#include "init.hpp"
+
 int main(int argc, char** argv)
 {
+    int nx = 100; // Size
+    int inter = nx / 2; // Interface
+
     Kokkos::ScopeGuard guard;
+    Kokkos::View<double*> rho("rho", nx); /**< Density */
+    Kokkos::View<double*> rhou("rhou", nx); /**< Momentum */
+    Kokkos::View<double*> E("E", nx); /**< Energy */
+
+    ShockTubeInit(rho, rhou, E, inter, nx);
+
+    Kokkos::parallel_for("", nx, KOKKOS_LAMBDA(int i){
+      printf("%f\n", rho(i));
+    });
+
     std::cout << "Hello world\n";
     return 0;
 }
