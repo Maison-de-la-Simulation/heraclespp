e0c674833e3a5e7e6a6916e4b13148005e37eba6
lrousselhard <lou.roussel-hard@cea.fr>
2023-01-06 13:46:44 +0100

    Changes

--- src/init.hpp
+++ src/init.hpp
@@ -7,14 +7,14 @@
 #include <Kokkos_Core.hpp>
 
 //! Initialisation for the schock tube problem
-//! @param[out] rho density array
+//! @param[inout] rho density array
 //! @param[out] u speed array
 //! @param[out] P pressue array
 //! @param[in] inter interface value
 //! @param[in] nx number of cells
 void ShockTubeInit(
         Kokkos::View<double*> rho,
-        Kokkos::View<double*> rhou,
-        Kokkos::View<double*> E,
+        Kokkos::View<double*> u,
+        Kokkos::View<double*> P,
         int inter,
         int nx);
