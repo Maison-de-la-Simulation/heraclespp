45f11aff2c2f0c49a89867ae6b58d8db27fe3aae
lrousselhard <lou.roussel-hard@cea.fr>
2023-01-06 13:46:41 +0100

    Changes

--- src/init.cpp
+++ src/init.cpp
@@ -2,6 +2,18 @@
 
 #include "init.hpp"
 
+// Global variables
+// Left side
+double const rhoL = 1; // Density
+double const uL = 0; // Speed
+double const PL = 1; // Pressure
+// Right side
+double const rhoR = 0.125;
+double const uR = 0;
+double const PR = 0.1;
+
+//extern double global_a;
+
 void ShockTubeInit(
         Kokkos::View<double*> const rho,
         Kokkos::View<double*> const u,
@@ -9,15 +21,6 @@ void ShockTubeInit(
         int const nx,
         int const inter)
 {
-    // Left side
-    double const rhoL = 1; // Density
-    double const uL = 0; // Speed
-    double const PL = 1; // Pressure
-    // Right side
-    double const rhoR = 0.125;
-    double const uR = 0;
-    double const PR = 0.1;
-
     Kokkos::parallel_for(
             "ShockTubeInit",
             nx,
