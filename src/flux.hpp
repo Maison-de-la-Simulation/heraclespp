d5f9e52b572e6b69be14743aa4e04a6f15ec9935
lrousselhard <lou.roussel-hard@cea.fr>
2023-01-12 17:03:22 +0100

    Add namespace

--- src/flux.hpp
+++ src/flux.hpp
@@ -8,22 +8,19 @@
 //! @param[in] rho double density
 //! @param[in] u double speed
 //! @param[in] P double pressure
-//! @param[in] gamma double
 //! @return flux
 class Flux
 {
-private:
+private :
       double m_rho;
       double m_u;
       double m_P;
-      double m_gamma;
 
 public :
       Flux(
             double const rho,
             double const u,
-            double const P,
-            double const gamma);
+            double const P);
       double FluxRho();
       double FluxRhou();
       double FluxE();
