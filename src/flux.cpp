0d09b644aae49beb9d671468acb4e37b5a0f6863
lrousselhard <lou.roussel-hard@cea.fr>
2023-01-12 17:03:17 +0100

    Add namespace

--- src/flux.cpp
+++ src/flux.cpp
@@ -1,15 +1,15 @@
 # include "flux.hpp"
+#include "global_var.hpp"
+using namespace GV;
 
 Flux::Flux(
         double const rho,
         double const u,
-        double const P,
-        double const gamma)
+        double const P)
 {
         m_rho = rho;
         m_u = u;
         m_P = P;
-        m_gamma = gamma;
 };
 
 double Flux::FluxRho()
@@ -24,5 +24,5 @@ double Flux::FluxRhou()
 
 double Flux::FluxE()
 {
-    return m_u * (1. / 2) * m_rho * m_u * m_u + m_P / (m_gamma - 1) + m_P;
+    return m_u * (1. / 2) * m_rho * m_u * m_u + m_P / (GV::gamma - 1) + m_P;
 }
