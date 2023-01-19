271860d882671bac8f7d6578929ea5a3423e102e
lrousselhard <lou.roussel-hard@cea.fr>
2023-01-19 14:08:11 +0100

    Indentation

--- src/fonc.cpp
+++ src/fonc.cpp
@@ -3,35 +3,35 @@
 #include "fonc.hpp"
 
 void ConvPrimCons(
-        Kokkos::View<double*> const rho,
-        Kokkos::View<double*> const rhou,
-        Kokkos::View<double*> const E,
-        Kokkos::View<double*> const u,
-        Kokkos::View<double*> const P,
-        double const gamma)
+    Kokkos::View<double*> const rho,
+    Kokkos::View<double*> const rhou,
+    Kokkos::View<double*> const E,
+    Kokkos::View<double*> const u,
+    Kokkos::View<double*> const P,
+    double const gamma)
 {
     Kokkos::parallel_for(
-            "ConvPrimCons",
-            rho.extent(0),
-            KOKKOS_LAMBDA(int i) {
-              rhou(i) = rho(i) * u(i) ;
-              E(i) = (1. / 2) * rho(i) * u(i) * u(i) + P(i) / (gamma - 1);
-            });
+        "ConvPrimCons",
+        rho.extent(0),
+        KOKKOS_LAMBDA(int i) {
+            rhou(i) = rho(i) * u(i) ;
+            E(i) = (1. / 2) * rho(i) * u(i) * u(i) + P(i) / (gamma - 1);
+        });
 }
 
 void ConvConsPrim(
-        Kokkos::View<double*> const rho,
-        Kokkos::View<double*> const rhou,
-        Kokkos::View<double*> const E,
-        Kokkos::View<double*> const u,
-        Kokkos::View<double*> const P,
-        double const gamma)
+    Kokkos::View<double*> const rho,
+    Kokkos::View<double*> const rhou,
+    Kokkos::View<double*> const E,
+    Kokkos::View<double*> const u,
+    Kokkos::View<double*> const P,
+    double const gamma)
 {
     Kokkos::parallel_for(
-            "ConvConsPrim",
-            rho.extent(0),
-            KOKKOS_LAMBDA(int i) {
-              u(i) = rhou(i) / rho(i);
-              P(i) = (gamma - 1) * (E(i) - (1. / 2) *rho(i) * u(i) * u(i));
-            });
+        "ConvConsPrim",
+        rho.extent(0),
+        KOKKOS_LAMBDA(int i) {
+            u(i) = rhou(i) / rho(i);
+            P(i) = (gamma - 1) * (E(i) - (1. / 2) * rho(i) * u(i) * u(i));
+        });
 }
