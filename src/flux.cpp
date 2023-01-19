7af0b6db31c693ccf0267b23e586eca02892026d
lrousselhard <lou.roussel-hard@cea.fr>
2023-01-19 14:08:00 +0100

    Indentation

--- src/flux.cpp
+++ src/flux.cpp
@@ -3,18 +3,18 @@
 using namespace GV;
 
 Flux::Flux(
-        double const rho,
-        double const u,
-        double const P)
+    double const rho,
+    double const u,
+    double const P)
 {
-        m_rho = rho;
-        m_u = u;
-        m_P = P;
+    m_rho = rho;
+    m_u = u;
+    m_P = P;
 };
 
 double Flux::FluxRho()
 {
-        return m_rho * m_u;
+    return m_rho * m_u;
 }
 
 double Flux::FluxRhou()
@@ -24,5 +24,5 @@ double Flux::FluxRhou()
 
 double Flux::FluxE()
 {
-    return m_u * (1. / 2) * m_rho * m_u * m_u + m_P / (GV::gamma - 1) + m_P;
+    return m_u * ((1. / 2) * m_rho * m_u * m_u + m_P / (GV::gamma - 1) + m_P);
 }
