09bd6b396bdbabf8603f3ebe7f9f5e1fa7e1eb1d
lrousselhard <lou.roussel-hard@cea.fr>
2023-01-19 14:07:06 +0100

    Boundary condition

--- src/boundary.hpp
+++ src/boundary.hpp
@@ -3,3 +3,11 @@
  * Boundary condition
  */
 #pragma once
+
+#include <Kokkos_Core.hpp>
+
+void GradientNull(
+    Kokkos::View<double *> const rho,
+    Kokkos::View<double *> const rhou,
+    Kokkos::View<double *> const E,
+    double size);
