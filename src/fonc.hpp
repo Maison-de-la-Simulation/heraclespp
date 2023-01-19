391cad4e4e2bb5a7936d81bb2a30d8cb3c2682ce
lrousselhard <lou.roussel-hard@cea.fr>
2023-01-19 14:08:08 +0100

    Indentation

--- src/fonc.hpp
+++ src/fonc.hpp
@@ -14,12 +14,12 @@
 //! @param[in] P pressure array
 //! @param[in] gamma
 void ConvPrimCons(
-        Kokkos::View<double*> const rho,
-        Kokkos::View<double*> const rhou,
-        Kokkos::View<double*> const E,
-        Kokkos::View<double*> const u,
-        Kokkos::View<double*> const P,
-        double const gamma);
+    Kokkos::View<double*> const rho,
+    Kokkos::View<double*> const rhou,
+    Kokkos::View<double*> const E,
+    Kokkos::View<double*> const u,
+    Kokkos::View<double*> const P,
+    double const gamma);
 
 //! Conversion conservative to primary variables
 //! @param[inout] rho density array
@@ -29,9 +29,9 @@ void ConvPrimCons(
 //! @param[in] P pressure array
 //! @param[in] gamma
 void ConvConsPrim(
-        Kokkos::View<double*> const rho,
-        Kokkos::View<double*> const rhou,
-        Kokkos::View<double*> const E,
-        Kokkos::View<double*> const u,
-        Kokkos::View<double*> const P,
-        double const gamma);
+    Kokkos::View<double*> const rho,
+    Kokkos::View<double*> const rhou,
+    Kokkos::View<double*> const E,
+    Kokkos::View<double*> const u,
+    Kokkos::View<double*> const P,
+    double const gamma);
