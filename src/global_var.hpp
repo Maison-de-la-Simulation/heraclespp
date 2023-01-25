a64a3a79fd7e9e59a245fcc4e447161e39a936e7
Thomas Padioleau <thomas.padioleau@cea.fr>
2023-01-25 17:27:21 +0100

    Resolve "Read gamma from .ini file"

--- src/global_var.hpp
+++ src/global_var.hpp
@@ -9,15 +9,7 @@
 
 // Transformer en structure. Mettre dans un fichier EOS.
 namespace GV{
-    const double gamma = 5. / 3;
-    const double g1 = (gamma - 1) / (2 * gamma);
-    const double g2 = (gamma + 1) / (2 * gamma);
-    const double g3 = 2 * gamma / (gamma - 1);
-    const double g4 = 2 / (gamma - 1);
-    const double g5 = 2 / (gamma + 1);
-    const double g6 = (gamma - 1) / (gamma + 1);
-    const double g7 = (gamma - 1) / 2;
-    const double g8 = (gamma - 1);
+    inline double gamma;
 }
 
 struct Grid {
