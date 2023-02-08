36ce9a89ab748c74fc96284af9aa58089a510ca6
Yushan Wang <yushan.wang@cea.fr>
2023-02-08 16:06:13 +0100

    update some doc

--- src/buffer.hpp
+++ src/buffer.hpp
@@ -11,74 +11,107 @@
 #include <Kokkos_Core.hpp>
 #include <Kokkos_DualView.hpp>
 #include "grid.hpp"
-      
-enum CORNERS
-{
-      LOWER_LEFT_FRONT  = 0,
-      LOWER_RIGHT_FRONT = 1,
-      UPPER_LEFT_FRONT  = 2,
-      UPPER_RIGHT_FRONT = 3,
-      LOWER_LEFT_BACK   = 4,
-      LOWER_RIGHT_BACK  = 5,
-      UPPER_LEFT_BACK   = 6,
-      UPPER_RIGHT_BACK  = 7,
-};
-
-
-enum EDGES
-{
-      EDGE01 = 0,
-      EDGE23 = 1,
-      EDGE45 = 2,
-      EDGE67 = 3,
-      EDGE02 = 4,
-      EDGE13 = 5,
-      EDGE46 = 6,
-      EDGE57 = 7,
-      EDGE04 = 8,
-      EDGE15 = 9,
-      EDGE26 = 10,
-      EDGE37 = 11,
-};
-
-enum FACES
-{
-      LEFT=0,
-      RIGHT=1,
-      LOWER=2,
-      UPPER=3,
-      FRONT=4,
-      BACK=5,
-};
-
 
 class Buffer
 {
 public :
-      int Nghost;
-      std::array<std::array<Kokkos::DualView<double ****>,2>,3> faceBuffer;
-      std::array<std::array<std::array<Kokkos::DualView<double ****>,2>,2>,3> edgeBuffer;
-      std::array<std::array<std::array<Kokkos::DualView<double ****>,2>,2>,2> cornerBuffer;
-
-      std::array<int, 3> faceBufferSize;
-      std::array<int, 3> edgeBufferSize;
-      int                cornerBufferSize;
+    int Nghost;
+    std::array<std::array<Kokkos::DualView<double ****>,2>,3> faceBuffer;
+    std::array<std::array<std::array<Kokkos::DualView<double ****>,2>,2>,3> edgeBuffer;
+    std::array<std::array<std::array<Kokkos::DualView<double ****>,2>,2>,2> cornerBuffer;
+    
+    std::array<int, 3> faceBufferSize;
+    std::array<int, 3> edgeBufferSize;
+    int                cornerBufferSize;
 public :
-      Buffer(Grid *grid, int nvar);
+    //!
+    //! @fn Constructs buffer used for border exchange
+    //! @brief Allocates buffer pf type Kokkos::DualView.
+    //! @param[in] grid Grid of simulation.
+    //! @param[in] nvar Number of variables needed for border exchanged.
+    //!
+    Buffer(Grid *grid, int nvar);
 };
 
+//! Update buffer from device data before border exchange
+//! @param[in] view Variable field device data
+//! @param[in] send_buffer Buffer used to store and later send the border value
+//! @param[in] ivar Index of input variable
+void copyToBuffer(Kokkos::View<double***> view, Buffer *send_buffer, int ivar);
 
-void copyToBuffer        (Kokkos::View<double***> view, Buffer *send_buffer, int ivar);
-void copyToBuffer_faces  (Kokkos::View<double***> view, Buffer *send_buffer, int ivar);
-void copyToBuffer_edges  (Kokkos::View<double***> view, Buffer *send_buffer, int ivar);
+
+//! Update face buffer from device data before border exchange
+//! @param[in] view Variable field device data
+//! @param[in] send_buffer Buffer used to store and later send the border value
+//! @param[in] ivar Index of input variable
+void copyToBuffer_faces(Kokkos::View<double***> view, Buffer *send_buffer, int ivar);
+
+
+//! Update edge buffer from device data before border exchange
+//! @param[in] view Variable field device data
+//! @param[in] send_buffer Buffer used to store and later send the border value
+//! @param[in] ivar Index of input variable
+void copyToBuffer_edges(Kokkos::View<double***> view, Buffer *send_buffer, int ivar);
+
+
+//! Update corner buffer from device data before border exchange
+//! @param[in] view Variable field device data
+//! @param[in] send_buffer Buffer used to store and later send the border value
+//! @param[in] ivar Index of input variable
 void copyToBuffer_corners(Kokkos::View<double***> view, Buffer *send_buffer, int ivar);
 
-void copyFromBuffer        (Kokkos::View<double***> view, Buffer *recv_buffer, int ivar);
-void copyFromBuffer_faces  (Kokkos::View<double***> view, Buffer *recv_buffer, int ivar);
-void copyFromBuffer_edges  (Kokkos::View<double***> view, Buffer *recv_buffer, int ivar);
+
+//! Update device data from buffer after border exchange
+//! @param[in] view Variable field device data
+//! @param[in] recv_buffer Buffer used to receive border value
+//! @param[in] ivar index of variable field
+void copyFromBuffer(Kokkos::View<double***> view, Buffer *recv_buffer, int ivar);
+
+
+//! Update device data from face buffer after border exchange
+//! @param[in] view Variable field device data
+//! @param[in] recv_buffer Buffer used to receive border value
+//! @param[in] ivar index of variable field
+void copyFromBuffer_faces(Kokkos::View<double***> view, Buffer *recv_buffer, int ivar);
+
+
+//! Update device data from edge buffer after border exchange
+//! @param[in] view Variable field device data
+//! @param[in] recv_buffer Buffer used to receive border value
+//! @param[in] ivar index of variable field
+void copyFromBuffer_edges(Kokkos::View<double***> view, Buffer *recv_buffer, int ivar);
+
+
+//! Update device data from corner buffer after border exchange
+//! @param[in] view Variable field device data
+//! @param[in] recv_buffer Buffer used to receive border value
+//! @param[in] ivar index of variable field
 void copyFromBuffer_corners(Kokkos::View<double***> view, Buffer *recv_buffer, int ivar);
 
+
+//! Performe the border value exchange
+//! @param[in] send_buffer Buffer used to send border value
+//! @param[in] recv_buffer Buffer used to receive border value
+//! @param[in] grid Grid of simulation.
 void exchangeBuffer(Buffer *send_buffer, Buffer *recv_buffer, Grid *grid);
+
+
+//! Performe the border face value exchange
+//! @param[in] send_buffer Buffer used to send border value
+//! @param[in] recv_buffer Buffer used to receive border value
+//! @param[in] grid Grid of simulation.
 void exchangeBuffer_faces(Buffer *send_buffer, Buffer *recv_buffer, Grid *grid);
+
+
+//! Performe the border ege value exchange
+//! @param[in] send_buffer Buffer used to send border value
+//! @param[in] recv_buffer Buffer used to receive border value
+//! @param[in] grid Grid of simulation.
 void exchangeBuffer_edges(Buffer *send_buffer, Buffer *recv_buffer, Grid *grid);
+
+
+//! Performe the border corner value exchange
+//! @param[in] send_buffer Buffer used to send border value
+//! @param[in] recv_buffer Buffer used to receive border value
+//! @param[in] grid Grid of simulation.
 void exchangeBuffer_corners(Buffer *send_buffer, Buffer *recv_buffer, Grid *grid);
