9b20f7291fa9b88e98b82fa39827abfa2585c4ef
Thomas Padioleau <thomas.padioleau@cea.fr>
2023-03-16 15:08:47 +0100

    Implement helper functions to call the kronecker

--- src/godunov_scheme.hpp
+++ src/godunov_scheme.hpp
@@ -104,12 +104,8 @@ public:
             }
             for (int idim = 0; idim < ndim; ++idim)
             {
-                int i_m = i - kron(idim, 0); // i - 1
-                int i_p = i + kron(idim, 0); // i + 1
-                int j_m = j - kron(idim, 1);
-                int j_p = j + kron(idim, 1);
-                int k_m = k - kron(idim, 2);
-                int k_p = k + kron(idim, 2);
+                auto const [i_m, j_m, k_m] = lindex(idim, i, j, k); // i - 1
+                auto const [i_p, j_p, k_p] = rindex(idim, i, j, k); // i + 1
 
                 EulerCons minus_oneR; // Right, back, top (i,j,k) - 1
                 minus_oneR.density = rho_rec(i_m, j_m, k_m, 1, idim);
---
+++ tests/kronecker.cpp
@@ -0,0 +1,59 @@
+#include <gtest/gtest.h>
+
+#include <kronecker.hpp>
+
+TEST(Kronecker, Kronecker)
+{
+    ASSERT_EQ(novapp::kron(1, 1), 1);
+    ASSERT_EQ(novapp::kron(1, 2), 0);
+    ASSERT_EQ(novapp::kron(2, 1), 0);
+    ASSERT_EQ(novapp::kron(2, 2), 1);
+}
+
+TEST(Kronecker, LIndex)
+{
+    {
+        auto const [i, j, k] = novapp::lindex(0, 2, 1, 1);
+        ASSERT_EQ(i, 1);
+        ASSERT_EQ(j, 1);
+        ASSERT_EQ(k, 1);
+    }
+
+    {
+        auto const [i, j, k] = novapp::lindex(1, 1, 2, 1);
+        ASSERT_EQ(i, 1);
+        ASSERT_EQ(j, 1);
+        ASSERT_EQ(k, 1);
+    }
+
+    {
+        auto const [i, j, k] = novapp::lindex(2, 1, 1, 2);
+        ASSERT_EQ(i, 1);
+        ASSERT_EQ(j, 1);
+        ASSERT_EQ(k, 1);
+    }
+}
+
+TEST(Kronecker, RIndex)
+{
+    {
+        auto const [i, j, k] = novapp::rindex(0, 0, 1, 1);
+        ASSERT_EQ(i, 1);
+        ASSERT_EQ(j, 1);
+        ASSERT_EQ(k, 1);
+    }
+
+    {
+        auto const [i, j, k] = novapp::rindex(1, 1, 0, 1);
+        ASSERT_EQ(i, 1);
+        ASSERT_EQ(j, 1);
+        ASSERT_EQ(k, 1);
+    }
+
+    {
+        auto const [i, j, k] = novapp::rindex(2, 1, 1, 0);
+        ASSERT_EQ(i, 1);
+        ASSERT_EQ(j, 1);
+        ASSERT_EQ(k, 1);
+    }
+}
