13c7feba9266c821f12c5f66b2f6db498fdd0377
Thomas Padioleau <thomas.padioleau@cea.fr>
2023-06-28 16:31:48 +0200

    Avoid to copy CPU-based objects

--- src/boundary.hpp
+++ src/boundary.hpp
@@ -93,13 +93,14 @@ public:
         end[m_bc_idim] = begin[m_bc_idim] + ng;
 
         int const offset = m_bc_iface == 0 ? end[m_bc_idim] : begin[m_bc_idim] - 1;
+        int const& bc_idim = m_bc_idim;
         Kokkos::parallel_for(
         m_label,
         Kokkos::MDRangePolicy<Kokkos::Rank<3>>(begin, end),
-        KOKKOS_CLASS_LAMBDA(int i, int j, int k) 
+        KOKKOS_LAMBDA(int i, int j, int k) 
         {
             Kokkos::Array<int, 3> offsets {i, j, k};
-            offsets[m_bc_idim] = offset;
+            offsets[bc_idim] = offset;
             rho(i, j, k) = rho(offsets[0], offsets[1], offsets[2]);
             for (int n = 0; n < rhou.extent_int(3); n++)
             {
@@ -166,19 +167,20 @@ public:
         end[m_bc_idim] = begin[m_bc_idim] + ng;
 
         int const mirror = m_bc_iface == 0 ? (2 * ng - 1) : (2 * (rho.extent(m_bc_idim) - ng) - 1);
+        int const& bc_idim = m_bc_idim;
         Kokkos::parallel_for(
         m_label,
         Kokkos::MDRangePolicy<Kokkos::Rank<3>>(begin, end),
-        KOKKOS_CLASS_LAMBDA(int i, int j, int k) 
+        KOKKOS_LAMBDA(int i, int j, int k) 
         {
             Kokkos::Array<int, 3> offsets {i, j, k};
-            offsets[m_bc_idim] = mirror - offsets[m_bc_idim];
+            offsets[bc_idim] = mirror - offsets[bc_idim];
             rho(i, j, k) = rho(offsets[0], offsets[1], offsets[2]);
             for (int n = 0; n < rhou.extent_int(3); n++)
             {
                 rhou(i, j, k, n) = rhou(offsets[0], offsets[1], offsets[2], n);
             }
-            rhou(i, j, k, m_bc_idim) = -rhou(i, j, k, m_bc_idim);
+            rhou(i, j, k, bc_idim) = -rhou(i, j, k, bc_idim);
             E(i, j, k) = E(offsets[0], offsets[1], offsets[2]);
             for (int ifx = 0; ifx < nfx; ++ifx)
             {
