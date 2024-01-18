91d6d3ea2de89eb766a24a080065bdd41cf42ef9
lrousselhard <lou.roussel-hard@cea.fr>
2024-01-18 11:06:44 +0100

    time step in spherical

--- src/hydro/time_step.cpp
+++ src/hydro/time_step.cpp
@@ -2,8 +2,10 @@
 
 #include <Kokkos_Core.hpp>
 #include <eos.hpp>
+#include <geom.hpp>
 #include <grid.hpp>
 #include <kronecker.hpp>
+#include <ndim.hpp>
 #include <range.hpp>
 
 #include "time_step.hpp"
@@ -31,13 +33,28 @@ double time_step(
         {
             Kokkos::Array<int, 3> const ijk {i,j,k};
             double const sound = eos.compute_speed_of_sound(rho(i, j, k), P(i, j, k));
-
+            Kokkos::Array<double, 3> dx_geom {0, 0, 0};
+            if (geom == Geometry::Geom_cartesian)
+            {
+                for(int idim = 0; idim < ndim; ++idim)
+                {
+                    dx_geom[idim] = dx[idim](ijk[idim]);
+                }
+            }
+            if (geom == Geometry::Geom_spherical)
+            {
+                dx_geom[0] = grid.dx(i);
+                if (ndim == 3)
+                {
+                    dx_geom[1] = grid.x_center(i) * grid.dy(j);
+                    dx_geom[2] = grid.x_center(i) * Kokkos::sin(grid.y_center(j)) * grid.dz(k);
+                }
+            }
             double cell_inverse_dt = 0;
             for(int idim = 0; idim < ndim; ++idim)
             {
-                cell_inverse_dt += (Kokkos::abs(u(i, j, k, idim)) + sound) / dx[idim](ijk[idim]);
+                cell_inverse_dt += (Kokkos::abs(u(i, j, k, idim)) + sound) / dx_geom[idim];
             }
-
             local_inverse_dt = Kokkos::max(local_inverse_dt, cell_inverse_dt);
         },
         Kokkos::Max<double>(inverse_dt));
