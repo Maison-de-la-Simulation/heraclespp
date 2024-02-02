75c359d37163bca8195f4f54895ae3b2d381baaf
Lou Roussel-Hard <lou.roussel-hard@cea.fr>
2024-02-02 14:07:06 +0100

    Resolve "add directory for output files"

--- src/io/io.hpp
+++ src/io/io.hpp
@@ -29,45 +29,55 @@ public:
     [[nodiscard]] bool operator()(int iter, double current, double dt) const;
 };
 
-void write_pdi_init(int max_iter, int frequency, Grid const& grid, Param const& param);
+void write_pdi_init(
+    std::string directory,
+    int max_iter,
+    int frequency,
+    Grid const& grid,
+    Param const& param);
 
-void write_pdi(int iter,
-               double t,
-               double gamma,
-               KDV_double_3d rho,
-               KDV_double_4d u,
-               KDV_double_3d P,
-               KDV_double_3d E,
-               KDV_double_1d x,
-               KDV_double_1d y,
-               KDV_double_1d z,
-               KDV_double_4d fx,
-               KDV_double_3d T);
+void write_pdi(
+    std::string directory,
+    int iter,
+    double t,
+    double gamma,
+    KDV_double_3d rho,
+    KDV_double_4d u,
+    KDV_double_3d P,
+    KDV_double_3d E,
+    KDV_double_1d x,
+    KDV_double_1d y,
+    KDV_double_1d z,
+    KDV_double_4d fx,
+    KDV_double_3d T);
 
-void read_pdi(std::string restart_file,
-              int& iter,
-              double& t,
-              KDV_double_3d rho,
-              KDV_double_4d u,
-              KDV_double_3d P,
-              KDV_double_4d fx,
-              KDV_double_1d x_glob,
-              KDV_double_1d y_glob,
-              KDV_double_1d z_glob);
+void read_pdi(
+    std::string restart_file,
+    int& iter,
+    double& t,
+    KDV_double_3d rho,
+    KDV_double_4d u,
+    KDV_double_3d P,
+    KDV_double_4d fx,
+    KDV_double_1d x_glob,
+    KDV_double_1d y_glob,
+    KDV_double_1d z_glob);
 
-void read_pdi_1d(std::string restart_file,
-              int& iter,
-              double& t,
-              KDV_double_1d rho,
-              KDV_double_1d u,
-              KDV_double_1d P,
-              KDV_double_2d fx,
-              KDV_double_1d x_glob);
+void read_pdi_1d(
+    std::string restart_file,
+    int& iter,
+    double& t,
+    KDV_double_1d rho,
+    KDV_double_1d u,
+    KDV_double_1d P,
+    KDV_double_2d fx,
+    KDV_double_1d x_glob);
 
 void writeXML(
-        Grid const& grid,
-        std::vector<std::pair<int, double>> const& outputs_record,
-        KDV_double_1d x,
-        KDV_double_1d y,
-        KDV_double_1d z);
+    Grid const& grid,
+    std::vector<std::pair<int, double>> const& outputs_record,
+    std::string directory,
+    KDV_double_1d x,
+    KDV_double_1d y,
+    KDV_double_1d z);
 }
