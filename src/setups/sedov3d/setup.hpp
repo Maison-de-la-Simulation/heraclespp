6c96b8fbf4145021bc5a964a22c1d2b66f20451f
lrousselhard <lou.roussel-hard@cea.fr>
2024-02-13 18:58:57 +0100

    review sedov 3D init

--- src/setups/sedov3d/setup.hpp
+++ src/setups/sedov3d/setup.hpp
@@ -11,6 +11,7 @@
 #include "default_user_step.hpp"
 #include "eos.hpp"
 #include <grid.hpp>
+#include <geom.hpp>
 #include "initialization_interface.hpp"
 #include "kokkos_shortcut.hpp"
 #include "ndim.hpp"
@@ -24,7 +25,6 @@ class ParamSetup
 {
 public:
     double rho0;
-    double u0;
     double P0;
     double E1;
     double ny;
@@ -32,7 +32,6 @@ public:
 
     explicit ParamSetup(INIReader const& reader)
         : rho0(reader.GetReal("Initialisation", "rho0", 1.0))
-        , u0(reader.GetReal("Initialisation", "u0", 1.0))
         , P0(reader.GetReal("Initialisation", "P0", 1.0))
         , E1(reader.GetReal("Initialisation", "E1", 1.0))
         , ny(reader.GetReal("Grid", "Ny_glob", 1.0))
@@ -85,6 +84,12 @@ public:
         int ny_2 = param_setup.ny / 2;
         int nz_2 = param_setup.nz / 2;
 
+
+        if (geom == Geometry::Geom_cartesian)
+        {
+            throw std::runtime_error("No Sedov 3D in Cartesian implemented");
+        }
+
         Kokkos::parallel_for(
             "Sedov_3D_init",
             cell_mdrange(range),
@@ -93,10 +98,10 @@ public:
                 rho(i, j, k) = param_setup.rho0;
                 for (int idim = 0; idim < ndim; ++idim)
                 {
-                    u(i, j, k, idim) = param_setup.u0;
+                    u(i, j, k, idim) = 0;
                 }
 
-                if (grid.mpi_rank == 0 && r(i) == 1 && j == ny_2  && k == nz_2)
+                if (r(i) == 1 && j == ny_2  && k == nz_2)
                 {
                     double evol = param_setup.E1 / dv(i, j, k);
                     P(i, j, k) = eos.compute_P_from_evol(rho(i, j, k), evol);
