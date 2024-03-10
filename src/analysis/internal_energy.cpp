47bfe9c8aceb34d875bc7e4e58dea408399594d2
Thomas Padioleau <thomas.padioleau@cea.fr>
2024-03-10 14:13:15 +0100

    Move analysis inline functions into source files

---
+++ src/analysis/internal_energy.cpp
@@ -0,0 +1,45 @@
+#include <euler_equations.hpp>
+#include <grid.hpp>
+#include <kokkos_shortcut.hpp>
+#include <ndim.hpp>
+#include <range.hpp>
+
+#include "internal_energy.hpp"
+
+namespace novapp
+{
+
+double internal_energy(
+    Range const& range,
+    Grid const& grid,
+    KV_cdouble_3d const rho,
+    KV_cdouble_4d const rhou,
+    KV_cdouble_3d const E)
+{
+    double evol = 0;
+
+    Kokkos::parallel_reduce(
+        "min_internal_energy",
+        cell_mdrange(range),
+        KOKKOS_LAMBDA(int i, int j, int k, double& local_evol)
+        {
+            EulerCons cons;
+            cons.rho = rho(i, j, k);
+            for (int idim = 0; idim < ndim; ++idim)
+            {
+                cons.rhou[idim] = rhou(i, j, k, idim);
+            }
+            cons.E = E(i, j, k);
+
+            double const cell_evol = compute_evol(cons);
+
+            local_evol = Kokkos::min(local_evol, cell_evol);
+        },
+        Kokkos::Min<double>(evol));
+
+    MPI_Allreduce(MPI_IN_PLACE, &evol, 1, MPI_DOUBLE, MPI_MIN, grid.comm_cart);
+
+    return evol;
+}
+
+} // namespace novapp
--- src/analysis/internal_energy.hpp
+++ src/analysis/internal_energy.hpp
@@ -4,46 +4,19 @@
 
 #pragma once
 
-#include <euler_equations.hpp>
-#include <grid.hpp>
 #include <kokkos_shortcut.hpp>
-#include <ndim.hpp>
-#include <range.hpp>
 
 namespace novapp
 {
 
-inline double internal_energy(
+class Range;
+class Grid;
+
+[[nodiscard]] double internal_energy(
     Range const& range,
     Grid const& grid,
-    KV_cdouble_3d const rho,
-    KV_cdouble_4d const rhou,
-    KV_cdouble_3d const E)
-{
-    double evol = 0;
-
-    Kokkos::parallel_reduce(
-        "min_internal_energy",
-        cell_mdrange(range),
-        KOKKOS_LAMBDA(int i, int j, int k, double& local_evol)
-        {
-            EulerCons cons;
-            cons.rho = rho(i, j, k);
-            for (int idim = 0; idim < ndim; ++idim)
-            {
-                cons.rhou[idim] = rhou(i, j, k, idim);
-            }
-            cons.E = E(i, j, k);
-
-            double const cell_evol = compute_evol(cons);
-
-            local_evol = Kokkos::min(local_evol, cell_evol);
-        },
-        Kokkos::Min<double>(evol));
-
-    MPI_Allreduce(MPI_IN_PLACE, &evol, 1, MPI_DOUBLE, MPI_MIN, grid.comm_cart);
-
-    return evol;
-}
+    KV_cdouble_3d rho,
+    KV_cdouble_4d rhou,
+    KV_cdouble_3d E);
 
 } // namespace novapp
