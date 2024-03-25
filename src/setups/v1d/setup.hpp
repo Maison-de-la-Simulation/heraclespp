fb0e625bf2dac3430199a51519c1c3c3a2d02c81
lrousselhard <lou.roussel-hard@cea.fr>
2024-03-25 16:06:38 +0100

    typo setupp 4

--- src/setups/v1d/setup.hpp
+++ src/setups/v1d/setup.hpp
@@ -36,13 +36,13 @@ public:
 
     explicit ParamSetup(INIReader const& reader)
 
-        : reader.Get("problem", "init_file", "")
-        , reader.GetReal("Grid", "xmin", 0.0)
-        , reader.GetReal("Grid", "ymin", 0.0)
-        , reader.GetReal("Grid", "ymax", 1.0)
-        , reader.GetReal("Grid", "zmin", 0.0)
-        , reader.GetReal("Grid", "zmax", 1.0)
-        , reader.GetReal("Initialisation", "xchoc", 1.0)
+        : init_filename(reader.Get("problem", "init_file", ""))
+        , xmin(reader.GetReal("Grid", "xmin", 0.0))
+        , ymin(reader.GetReal("Grid", "ymin", 0.0))
+        , ymax(reader.GetReal("Grid", "ymax", 1.0))
+        , zmin(reader.GetReal("Grid", "zmin", 0.0))
+        , zmax(reader.GetReal("Grid", "zmax", 1.0))
+        , xchoc(reader.GetReal("Initialisation", "xchoc", 1.0))
         {}
 };
 
