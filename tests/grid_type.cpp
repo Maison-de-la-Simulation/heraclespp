fae1e89938e293155f7cb6e5eb42d770b9cfc1d2
Thomas Padioleau <thomas.padioleau@cea.fr>
2024-03-30 20:46:46 +0100

    Add test for compute_regular_mesh_1d

---
+++ tests/grid_type.cpp
@@ -0,0 +1,15 @@
+#include <gtest/gtest.h>
+
+#include <grid_type.hpp>
+
+TEST(GridType, ComputeRegularMesh1d)
+{
+    int const hw = 1;
+    Kokkos::View<double*, Kokkos::HostSpace> const x("x", 3 + 2 * hw);
+    novapp::compute_regular_mesh_1d(x, hw, -1., 1.);
+    EXPECT_DOUBLE_EQ(x(0), -2.);
+    EXPECT_DOUBLE_EQ(x(1), -1.);
+    EXPECT_DOUBLE_EQ(x(2), 0.);
+    EXPECT_DOUBLE_EQ(x(3), +1.);
+    EXPECT_DOUBLE_EQ(x(4), +2.);
+}
