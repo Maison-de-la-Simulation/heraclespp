223e3d8c6e7a8b01a4e77a781572a1f3f8287721
Thomas Padioleau <thomas.padioleau@cea.fr>
2024-04-10 21:45:25 +0200

    Fix error computation in the convergence test

--- test/run_convergence_advection_gaussian.sh
+++ test/run_convergence_advection_gaussian.sh
@@ -23,17 +23,26 @@
 #mpiexec -n 2 ./$BINARY_NAME ./$INI_FILENAME ./$PDI_YML_FILENAME
 #python3 ../test/check_convergence_advection_sinusoide.py
 
-set -ex
-
 TEST_DIR_NAME=convergence_advection_gaussian
 PDI_YML_FILENAME=convergence_advection_gaussian.yml
 BINARY_NAME=nova++
 INI_FILENAME=advection_gaussian.ini
 
-mkdir $TEST_DIR_NAME && cd $TEST_DIR_NAME
-cp ../inputs/$INI_FILENAME .
-cp ../bin/$BINARY_NAME .
-cp ../test/$PDI_YML_FILENAME .
+TEST_DIR_NAME=$(mktemp -d)
+if [ $? -ne 0 ]; then
+    echo "$0: Can't create temporary directory, exiting."
+    exit 1
+fi
+
+trap 'rm -rf -- "$TEST_DIR_NAME"' EXIT
+set -ex
+
+cp ./inputs/$INI_FILENAME $TEST_DIR_NAME
+cp ./bin/$BINARY_NAME $TEST_DIR_NAME
+cp ./test/$PDI_YML_FILENAME $TEST_DIR_NAME
+cp ./test/check_convergence_advection_gaussian.py $TEST_DIR_NAME
+
+cd $TEST_DIR_NAME
 sed -i.bak 's/Nx_glob = .*/Nx_glob = 50/g' ./$INI_FILENAME
 mpiexec -n 2 ./$BINARY_NAME ./$INI_FILENAME ./$PDI_YML_FILENAME
 sed -i.bak 's/Nx_glob = .*/Nx_glob = 100/g' ./$INI_FILENAME
@@ -44,4 +53,9 @@ sed -i.bak 's/Nx_glob = .*/Nx_glob = 400/g' ./$INI_FILENAME
 mpiexec -n 2 ./$BINARY_NAME ./$INI_FILENAME ./$PDI_YML_FILENAME
 sed -i.bak 's/Nx_glob = .*/Nx_glob = 800/g' ./$INI_FILENAME
 mpiexec -n 2 ./$BINARY_NAME ./$INI_FILENAME ./$PDI_YML_FILENAME
-python3 ../test/check_convergence_advection_gaussian.py
+
+python3 ./check_convergence_advection_gaussian.py
+
+rm -rf -- "$TEST_DIR_NAME"
+
+trap - EXIT
