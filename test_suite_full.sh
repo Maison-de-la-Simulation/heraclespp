2d848725970361efd3d687b49ce805f3fdbe9354
Thomas Padioleau <thomas.padioleau@cea.fr>
2024-04-14 13:26:01 +0200

    Update test_suite_full.sh and make CMAKE_GENERATOR a global variable

--- test_suite_full.sh
+++ test_suite_full.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
+
 export CMAKE_BUILD_PARALLEL_LEVEL=4
 export CMAKE_BUILD_TYPE="Release"
-export CXXFLAGS="-Werror=all -Werror=extra -Werror=pedantic -pedantic-errors"
 
 DIRECTORY=$(mktemp -d)
 if [ $? -ne 0 ]; then
@@ -12,10 +12,23 @@ fi
 trap 'rm -rf -- "$DIRECTORY"' EXIT
 set -xe
 
-BUILD_DIRECTORY="$DIRECTORY/build"
+# Install Kokkos
+cmake -B "$DIRECTORY/build-kokkos" -S vendor/kokkos -DBUILD_TESTING=OFF -DKokkos_ENABLE_DEPRECATED_CODE_4=OFF
+cmake --build "$DIRECTORY/build-kokkos"
+cmake --install "$DIRECTORY/build-kokkos" --prefix "$DIRECTORY/install-kokkos"
+export Kokkos_ROOT="$DIRECTORY/install-kokkos"
+
+# Install GTest
+cmake -B "$DIRECTORY/build-gtest" -S vendor/googletest
+cmake --build "$DIRECTORY/build-gtest"
+cmake --install "$DIRECTORY/build-gtest" --prefix "$DIRECTORY/install-gtest"
+export GTest_ROOT="$DIRECTORY/install-gtest"
+
+export CXXFLAGS="-Werror=all -Werror=extra -Werror=pedantic -pedantic-errors"
+export BUILD_DIRECTORY="$DIRECTORY/build-novapp"
 
 # Warm up, first configuration with shared options
-cmake -B "$BUILD_DIRECTORY" -S . -DBUILD_TESTING=OFF -DNovapp_SETUP=advection_gap -DNovapp_NDIM=1 -DNovapp_EOS=PerfectGas -DNovapp_GRAVITY=Uniform -DNovapp_GEOM=Cartesian -DKokkos_ENABLE_DEPRECATED_CODE_4=OFF
+cmake -B "$BUILD_DIRECTORY" -DBUILD_TESTING=OFF -DNovapp_GTest_DEPENDENCY_POLICY=INSTALLED -DNovapp_Kokkos_DEPENDENCY_POLICY=INSTALLED -DNovapp_SETUP=advection_gap -DNovapp_NDIM=1 -DNovapp_EOS=PerfectGas -DNovapp_GRAVITY=Uniform -DNovapp_GEOM=Cartesian
 
 # Configure and build all setups
 cmake "$BUILD_DIRECTORY" -DNovapp_SETUP=advection_gap -DNovapp_NDIM=1 -DNovapp_EOS=PerfectGas -DNovapp_GRAVITY=Uniform -DNovapp_GEOM=Cartesian
