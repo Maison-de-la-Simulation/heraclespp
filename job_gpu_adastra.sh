9ecde132d1df1b941ee6e80e30a7489e655be67b
lroussel <lroussel@login2.head.adastra.cines.fr>
2024-04-25 11:19:13 +0200

    valeur M centrale et kokkos

--- job_gpu_adastra.sh
+++ job_gpu_adastra.sh
@@ -2,10 +2,10 @@
 #SBATCH --account=cin4698
 #SBATCH --job-name=nova-run
 #SBATCH --output=%x.o%j
-#SBATCH --nodes=2
+#SBATCH --nodes=1
 #SBATCH --exclusive
 #SBATCH --constraint=MI250
-#SBATCH --time=18:00:00
+#SBATCH --time=10:00:00
 
 # load modules here
 module load cpe/23.05 craype-x86-trento craype-accel-amd-gfx90a PrgEnv-gnu amd-mixed/5.5.1 rocm/5.2.3 cray-hdf5-parallel/1.12.2.1 cray-mpich/8.1.24
@@ -14,4 +14,4 @@ module load cpe/23.05 craype-x86-trento craype-accel-amd-gfx90a PrgEnv-gnu amd-m
 cd ${SLURM_SUBMIT_DIR}
 
 # execution
-srun --ntasks=16 --gpus-per-node=8 ./../bin/nova++ ../inputs/v1d_3d.ini  
+srun --ntasks=8 --gpus-per-node=8 ./../bin/nova++ ../inputs/v1d_3d_5e2_768p.ini  
