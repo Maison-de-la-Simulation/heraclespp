992895a52885d0f4f5f90b8762087e70ee4eb2aa
lroussel <lroussel@login2.head.adastra.cines.fr>
2024-08-08 10:59:33 +0200

    delete some ini files

--- job_gpu_adastra_256.sh
+++ job_gpu_adastra_256.sh
@@ -2,16 +2,16 @@
 #SBATCH --account=c1615137
 #SBATCH --job-name=nova-run
 #SBATCH --output=%x.o%j
-#SBATCH --nodes=4
+#SBATCH --nodes=8
 #SBATCH --exclusive
 #SBATCH --constraint=MI250
 #SBATCH --time=23:00:00
 
 # load modules here
-module load cpe/23.05 craype-x86-trento craype-accel-amd-gfx90a PrgEnv-gnu amd-mixed/5.5.1 rocm/5.2.3 cray-hdf5-parallel/1.12.2.1 cray-mpich/8.1.24
+module load cpe/23.12 craype-x86-trento craype-accel-amd-gfx90a PrgEnv-amd cray-hdf5-parallel
 
 # To compute in the submission directory
 cd ${SLURM_SUBMIT_DIR}
 
 # execution
-srun --ntasks=32 --gpus-per-node=8 ./../bin/nova++ ../inputs/v1d_3d_5e2_768p_256.ini  
+srun --ntasks=64 --gpus-per-node=8 ./../bin/nova++ ../inputs/v1d_3d_5e2_768p_1_256.ini
