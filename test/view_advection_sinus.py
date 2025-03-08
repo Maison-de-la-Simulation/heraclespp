11ae61bc34f3fd1031cc097c01c1d72e8e6ae64b
lrousselhard <lou.roussel-hard@cea.fr>
2025-03-08 18:34:07 +0100

    review advection sinus

--- test/view_advection_sinus.py
+++ test/view_advection_sinus.py
@@ -1,10 +1,9 @@
 # Test the advection sinusoide and compare
 
-import sys
-
 import h5py
 import matplotlib.pyplot as plt
 import numpy as np
+import sys
 
 print("********************************")
 print("  Advection test : sinus")
@@ -13,7 +12,6 @@ print("********************************")
 filename = sys.argv[1]
 
 with h5py.File(str(filename), 'r') as f:
-    #print(f.keys())
     rho = f['rho'][0, 0, :]
     x = f['x'][()]
     t = f['current_time'][()]
@@ -36,7 +34,7 @@ for i in range(2, len(rho)+2):
 rho0 = np.zeros(len(rho))
 
 for i in range(len(rho0)):
-    rho0[i] = 1 + 0.1 * np.sin(2 * np.pi * xc[i]) # Sinusoïdale
+    rho0[i] = 1 + np.sin(2 * np.pi * xc[i])
 
 # ------------------------------------------
 
@@ -44,9 +42,7 @@ plt.figure(figsize=(10,8))
 plt.title('Sinus advection test')
 plt.plot(xc, rho0, '--', label='t = 0')
 plt.plot(xc, rho, label=f't = {t:.1f}')
-plt.xticks([0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1])
 plt.xlabel('Position')
 plt.ylabel('Density')
-plt.grid()
-plt.legend()
+plt.legend(frameon=False)
 plt.show()
