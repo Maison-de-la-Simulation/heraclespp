cd135c56228eb713466b2f4e2567aea37d3258ad
lrousselhard <lou.roussel-hard@cea.fr>
2025-03-08 13:38:21 +0100

    review shock tube 1D cartesian

--- src/setups/shock_tube/setup.hpp
+++ src/setups/shock_tube/setup.hpp
@@ -7,6 +7,7 @@
 #include <inih/INIReader.hpp>
 
 #include "default_boundary_setup.hpp"
+#include "default_grid_setup.hpp"
 #include "default_shift_criterion.hpp"
 #include "default_user_step.hpp"
 #include "eos.hpp"
@@ -84,94 +85,25 @@ public:
             cell_mdrange(range),
             KOKKOS_LAMBDA(int i, int j, int k)
             {
-                if(xc(i) * units::m <= param_setup.inter)
+                if(xc(i) <= param_setup.inter)
                 {
-                    rho(i, j, k) = param_setup.rho0 * units::density;
-                    P(i, j, k) = param_setup.P0 * units::pressure;
+                    rho(i, j, k) = param_setup.rho0;
+                    P(i, j, k) = param_setup.P0;
                     for (int idim = 0; idim < ndim; ++idim)
                     {
-                        u(i, j, k, idim) = param_setup.u0 * units::velocity;
+                        u(i, j, k, idim) = param_setup.u0;
                     }
-                        fx(i, j, k, 0) = param_setup.fx0;
                 }
                 else
                 {
-                    rho(i, j, k) = param_setup.rho1 * units::density;
-                    P(i, j, k) = param_setup.P1 * units::pressure;
+                    rho(i, j, k) = param_setup.rho1;
+                    P(i, j, k) = param_setup.P1;
                     for (int idim = 0; idim < ndim; ++idim)
                     {
-                        u(i, j, k, idim) = param_setup.u1 * units::velocity;
+                        u(i, j, k, idim) = param_setup.u1;
                     }
-                        fx(i, j, k, 0) = param_setup.fx1;
                 }
             });
-    }
-};
-
-class GridSetup : public IGridType
-{
-private :
-    Param m_param;
-
-public:
-    explicit GridSetup(
-        Param param)
-        : m_param(std::move(param))
-    {
-    }
-
-    void execute(
-        std::array<int, 3> const Nghost,
-        std::array<int, 3> const Nx_glob_ng,
-        KVH_double_1d const& x_glob,
-        KVH_double_1d const& y_glob,
-        KVH_double_1d const& z_glob) const final
-    {
-        double const Lx = m_param.xmax - m_param.xmin;
-        double const dx = Lx / Nx_glob_ng[0];
-        x_glob(Nghost[0]) = m_param.xmin;
-
-        int const quarter_x = Nx_glob_ng[0] / 4;
-        int const three_quarters_x = 3 * quarter_x;
-
-        for (int i = Nghost[0]+1; i < x_glob.extent_int(0) ; ++i)
-        {
-            double dxloc;
-            if ((i >= quarter_x) && (i <= three_quarters_x))
-            {
-                dxloc = dx / 5;
-            }
-            else
-            {
-                dxloc = dx / 4;
-            }
-            x_glob(i) = x_glob(i-1) + dxloc;
-        }
-
-        double const val_xmax = x_glob(x_glob.extent_int(0)-1 - Nghost[0]);
-        for (int i = Nghost[0]; i < x_glob.extent_int(0); ++i)
-        {
-            x_glob(i) = m_param.xmax * x_glob(i) / val_xmax;
-        }
-
-        // Left ghost cells
-        for(int i = Nghost[0]-1; i >= 0; i--)
-        {
-            x_glob(i) = x_glob(i+1) - dx / 4;
-        }
-
-        // Y and Z
-        double const Ly = m_param.ymax - m_param.ymin;
-        double const dy = Ly / Nx_glob_ng[1];
-
-        double const Lz = m_param.zmax - m_param.zmin;
-        double const dz = Lz / Nx_glob_ng[2];
-
-        for (int i = 0; i < y_glob.extent_int(0) ; ++i)
-        {
-            y_glob(i) = m_param.ymin + i * dy;
-            z_glob(i) = m_param.zmin + i * dz;
-        }
     }
 };
 
--- test/view_shock_tube.py
+++ test/view_shock_tube.py
@@ -1,148 +1,110 @@
 # Test the shock tube problem and compare
-
-from argparse import ArgumentParser
-from pathlib import Path
-
-import h5py
-import matplotlib.pyplot as plt
 import numpy as np
+import matplotlib.pyplot as plt
+import h5py
+import sys
 
+from fonction import make_xc
 from exact_shock_tube import CI, ExactShockTube
 
-parser = ArgumentParser(description="Plot shock tube.")
-parser.add_argument('filename',
-                    type=Path,
-                    help='Input filename')
-parser.add_argument('-o', '--output',
-                    required=False,
-                    default=None,
-                    type=Path,
-                    help='Output file to generate')
-args = parser.parse_args()
-
 print("********************************")
-print("         Shock tube")
+print("    Shock tube 1D Cartesian")
 print("********************************")
 
-with h5py.File(args.filename, 'r') as f:
-    #print(f.keys())
-    rho = f['rho'][0, 0, :]
-    u = f['ux'][0, 0, :]
-    P = f['P'][0, 0, :]
-    x = f['x'][()]
-    fx = f['fx'][0, 0, 0, :]
-    T = f['T'][0, 0, :]
-    t = f['current_time'][()]
-    iter = f['iter'][()]
-    gamma = f['gamma'][()]
+file = sys.argv[1]
 
-print(f"Final time = {t:.1f} s")
-print(f"Iteration number = {iter}")
+def read_file(filename):
+    with h5py.File(filename, 'r') as f:
+        rho = f['rho'][0, 0, :]
+        u = f['ux'][0, 0, :]
+        P = f['P'][0, 0, :]
+        x = f['x'][()]
+        T = f['T'][0, 0, :]
+        t = f['current_time'][()]
+        iter = f['iter'][()]
+        gamma = f['gamma'][()]
+    e = P / rho / (gamma - 1)
+    nx = len(rho)
+    xc = make_xc(x, nx)
 
-print("Max fx =", np.max(fx))
+    print(f"Final time = {t:.1f} s")
+    print(f"Iteration number = {iter}")
 
-xmin = x[2]
-xmax = x[len(x)-3]
-L = xmax - xmin
+    return rho, u, P, e, xc, x, gamma, t
 
-e = P / rho / (gamma - 1)
+def analytical_result(x, gamma, t):
+    inter = 0.5 # Interface position
+    xmin = x[2]
+    xmax = x[len(x)-3]
+    L = xmax - xmin
 
-dx = np.zeros(len(rho))
-for i in range(2, len(rho)+2):
-    dx[i-2] = x[i+1] - x[i]
+    # Left
+    rho0l = 1
+    u0l = 0
+    P0l = 1
+    c0l = np.sqrt(gamma * P0l / rho0l)
+    var0l = np.array([rho0l, u0l, P0l, c0l])
 
-xc = np.zeros(len(rho))
-for i in range(2, len(rho)+2):
-    xc[i-2] = x[i] + dx[i-2] / 2
+    # Right
+    rho0r = 0.125
+    u0r = 0
+    P0r = 0.1
+    c0r = np.sqrt(gamma * P0r / rho0r)
+    var0r = np.array([rho0r, u0r, P0r, c0r])
 
-print("dx =", dx)
+    Ncell = 1_000
+    dx_exact = L / Ncell
+    x_exact = np.zeros(Ncell)
 
-# Analytical result ------------------------
+    for i in range(len(x_exact)):
+        x_exact[i] = i * dx_exact + dx_exact / 2
 
-inter = 0.5 # Interface position
+    rho0, u0, P0 = CI(x_exact, inter, var0l, var0r)
+    e0 = P0 / rho0 / (gamma - 1)
 
-# Left
-rho0l = 1
-u0l = 0
-P0l = 1
-c0l = np.sqrt(gamma * P0l / rho0l)
-var0l = np.array([rho0l, u0l, P0l, c0l])
+    rho_exact, u_exact, P_exact, e_exact = ExactShockTube(x_exact, inter, var0l, var0r, t, gamma)
 
-# Right
-rho0r = 0.125
-u0r = 0
-P0r = 0.1
-c0r = np.sqrt(gamma * P0r / rho0r)
-var0r = np.array([rho0r, u0r, P0r, c0r])
+    return rho0, u0, P0, e0, rho_exact, u_exact, P_exact, e_exact, x_exact
 
-Ncell = 1_000
-dx_exact = L / Ncell
-x_exact = np.zeros(Ncell)
+# ------------------------------------------------------------------------------
 
-for i in range(len(x_exact)):
-    x_exact[i] = i * dx_exact + dx_exact / 2
+rho, u, P, e, xc, x, gamma, t = read_file(file)
+rho0, u0, P0, e0, rho_exact, u_exact, P_exact, e_exact, x_exact = analytical_result(x, gamma, t)
 
-rho0, u0, P0 = CI(x_exact, inter, var0l, var0r)
-e0 = P0 / rho0 / (gamma - 1)
+# ------------------------------------------------------------------------------
 
-rho_exact, u_exact, P_exact, e_exact = ExactShockTube(x_exact, inter, var0l, var0r, t, gamma)
-
-# ------------------------------------------
-
-plt.figure(figsize=(10,8))
+plt.figure(figsize=(12, 6))
 plt.suptitle(f'Shock tube t = {t:.1f} s')
 plt.subplot(221)
-plt.plot(x_exact, rho0, '--', label='t=0')
+plt.plot(x_exact, rho0, '--', label='$t_0$')
 plt.plot(x_exact,rho_exact, label='Exact')
-plt.plot(xc, rho, label = 'Solver')#f't = {t:1f}')
-plt.plot(xc, fx, label='Scalar')
+plt.plot(xc, rho, label = 'Solver')
 plt.xlabel('Position')
 plt.ylabel('Density ($kg.m^{-3}$)')
-plt.xticks([0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1])
-plt.grid()
-plt.legend()
+plt.legend(frameon=False)
 
 plt.subplot(222)
-plt.plot(x_exact, u0, '--', label='t=0')
+plt.plot(x_exact, u0, '--', label='$t_0$')
 plt.plot(x_exact, u_exact, label='Exact')
-plt.plot(xc, u, 'x-', label = 'Solver')#f't = {t:1f}')
+plt.plot(xc, u, label = 'Solver')
 plt.xlabel('Position')
 plt.ylabel('Velocity ($m.s^{-1}$)')
-plt.xticks([0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1])
-plt.grid()
-plt.legend()
+plt.legend(frameon=False)
 
 plt.subplot(223)
-plt.plot(x_exact, P0,'--', label='t=0')
+plt.plot(x_exact, P0,'--', label='$t_0$')
 plt.plot(x_exact, P_exact, label='Exact')
-plt.plot(xc, P, 'x-', label = 'Solver')#f't = {t:1f}')
+plt.plot(xc, P, label = 'Solver')
 plt.xlabel('Position')
 plt.ylabel('Pressure ($kg.m^{-1}.s^{-2}$)')
-plt.xticks([0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1])
-plt.grid()
-plt.legend()
+plt.legend(frameon=False)
 
 plt.subplot(224)
-plt.plot(x_exact, e0,'--', label='t=0')
+plt.plot(x_exact, e0,'--', label='$t_0$')
 plt.plot(x_exact, e_exact, label='Exact')
-plt.plot(xc, e, 'x-', label = 'Solver')#f't = {t:1f}')
+plt.plot(xc, e, label = 'Solver')
 plt.xlabel('Position')
 plt.ylabel('Internal energy ($m^{2}.s^{-2}$)')
-plt.xticks([0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1])
-plt.grid()
-plt.legend()
+plt.legend(frameon=False)
 
-plt.figure()
-plt.title('Passive scalar')
-plt.plot(xc, fx)
-plt.grid()
-
-plt.figure()
-plt.title('Temperature')
-plt.plot(xc, T)
-plt.grid()
-
-if args.output is None:
-    plt.show()
-else:
-    plt.savefig(args.output)
+plt.show()
