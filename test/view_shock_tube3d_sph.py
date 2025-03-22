46fd32f08607440e1c5cff65cc5d55e9be08a224
lrousselhard <lou.roussel-hard@cea.fr>
2025-03-22 15:37:16 +0100

    test shock tube spherical 3D

--- test/view_shock_tube3d_sph.py
+++ test/view_shock_tube3d_sph.py
@@ -10,21 +10,13 @@ print("********************************")
 print("   Shock tube spherical 3d")
 print("********************************")
 
-with h5py.File("result_00000000.h5", 'r') as f:
-    #print(f.keys())
-    rho0 = f['rho'][0 , 0, :] # rho(r)
-    u0 = f['ux'][0, 0, :]
-    P0 = f['P'][0, 0, :]
-
 filename = sys.argv[1]
 
 with h5py.File(str(filename), 'r') as f:
-    #print(f.keys())
-    rho_1d = f['rho'][5 , 5, :] # rho(r)
+    rho_1d = f['rho'][0 , 0, :] # rho(r)
     rho2 = f['rho'][:, 0, :] # rho(r, phi)
     u_1d = f['ux'][0, 0, :]
     P_1d = f['P'][0, 0, :]
-    fx_1d = f['fx'][0, 0, 0, :]
     x = f['x'][()]
     y = f['y'][()]
     z = f['z'][()]
@@ -33,9 +25,7 @@ with h5py.File(str(filename), 'r') as f:
     gamma = f['gamma'][()]
 
 print(f"Final time = {t:.1f} s")
-print(f"Iteration number = {iter}")
 
-e0 = P0 / rho0 / (gamma - 1)
 e_1d = P_1d / rho_1d / (gamma - 1)
 
 rmin = x[2]
@@ -73,36 +63,23 @@ plt.colorbar()
 plt.figure(figsize=(10,8))
 plt.suptitle(f'Shock tube t = {t:.1f} s')
 plt.subplot(221)
-plt.plot(rc, rho0, "--", label="t=0")
-plt.plot(rc, rho_1d, label="3D spherical")
-plt.plot(rc, fx_1d, label="passive scalar")
-plt.grid()
+plt.plot(rc, rho_1d)
 plt.xlabel('Radius (m)')
 plt.ylabel(r'Density ($kg.m^{-3}$)')
-plt.legend()
 
 plt.subplot(222)
-plt.plot(rc, u0, "--", label="t=0")
-plt.plot(rc, u_1d, label="3D spherical")
-plt.grid()
+plt.plot(rc, u_1d)
 plt.xlabel('Radius (m)')
 plt.ylabel(r'Velocity ($m.s^{-1}$)')
-plt.legend()
 
 plt.subplot(223)
-plt.plot(rc, P0, "--", label="t=0")
-plt.plot(rc, P_1d, label="3D spherical")
-plt.grid()
+plt.plot(rc, P_1d)
 plt.xlabel('Radius (m)')
 plt.ylabel(r'Pressure ($kg.m^{-1}.s^{-2}$)')
-plt.legend()
 
 plt.subplot(224)
-plt.plot(rc, e0, "--", label="t=0")
-plt.plot(rc, e_1d, label="3D spherical")
-plt.grid()
+plt.plot(rc, e_1d)
 plt.xlabel('Radius (m)')
 plt.ylabel(r'Internal energy ($m^{2}.s^{-2}$)')
-plt.legend()
 
 plt.show()
