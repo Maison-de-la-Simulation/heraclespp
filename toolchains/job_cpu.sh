dc44a4bc90aabeaa30c26ded47074c1b62b2e748
Thomas Padioleau <thomas.padioleau@cea.fr>
2025-03-24 18:23:15 +0100

    Add shell workflow (#24)

--- toolchains/job_cpu.sh
+++ toolchains/job_cpu.sh
@@ -6,6 +6,8 @@
 #SBATCH --cpus-per-task=20              # thread number
 #SBATCH --partition=cpu_short           # (see available partitions)
 
+set -e
+
 # To clean and load modules defined at the compile and link phases
 module purge
 module load gcc/11.2.0/gcc-4.8.5 hdf5/1.10.7/gcc-11.2.0-openmpi openmpi/4.1.1/gcc-11.2.0 cuda/11.7.0/gcc-11.2.0 cmake/3.21.4/gcc-11.2.0
@@ -14,7 +16,7 @@ module load gcc/11.2.0/gcc-4.8.5 hdf5/1.10.7/gcc-11.2.0-openmpi openmpi/4.1.1/gc
 set -x
 
 # To compute in the submission directory
-cd ${SLURM_SUBMIT_DIR}
+cd "${SLURM_SUBMIT_DIR}"
 
 # number of OpenMP threads
 export OMP_NUM_THREADS=${SLURM_CPUS_PER_TASK}
